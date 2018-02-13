f402374fd2f72c3451b163f6fbe8ae77d70f4ad5
Adam Clarke <adam@mainframe.co>
2018-02-13 15:35:18 +0000

    deposit limit on stake contract, make stake contract ownable, frontend updates, tests

--- contracts/MainframeStake.sol
+++ contracts/MainframeStake.sol
@@ -1,24 +1,28 @@
 pragma solidity ^0.4.18;
 
+import "zeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
+
 contract MainframeToken {
   function transferFrom(address from, address to, uint256 value) public returns (bool);
   function transfer(address to, uint256 value) public returns (bool);
-	function balanceOf(address who) public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
 }
 
-contract MainframeStake {
+contract MainframeStake is Ownable {
   mapping (address => uint256) public balances;
   MainframeToken token;
-  uint256 public totalStaked;
+  uint256 public maxDeposit;
 
   function MainframeStake(address _tokenAddress) public {
     token = MainframeToken(_tokenAddress);
+    maxDeposit = 100;
+    owner = msg.sender;
   }
 
   function deposit(uint256 _value) public returns (bool success) {
+    require(balances[msg.sender] + _value <= maxDeposit);
     token.transferFrom(msg.sender, this, _value);
     balances[msg.sender] += _value;
-    totalStaked += _value;
     Deposit(msg.sender, _value, balances[msg.sender]);
     return true;
   }
@@ -27,7 +31,6 @@ contract MainframeStake {
     require(balances[msg.sender] >= _value);
     token.transfer(msg.sender, _value);
     balances[msg.sender] -= _value;
-    totalStaked -= _value;
     Withdrawal(msg.sender, _value, balances[msg.sender]);
     return true;
   }
@@ -41,11 +44,20 @@ contract MainframeStake {
   }
 
   function totalStaked() public view returns (uint256) {
-    return totalStaked;
+    return token.balanceOf(address(this));
   }
 
   function hasStake(address _address) public view returns (bool) {
     return balances[_address] > 0;
+  }
+
+  function maxDeposit() public view returns (uint256) {
+    return maxDeposit;
+  }
+
+  function setMaxDeposit(uint256 _value) public {
+    require(msg.sender == owner);
+    maxDeposit = _value;
   }
 
   event Deposit(address indexed _owner, uint256 _value, uint256 _balance);
--- test/TestMainframeStake.js
+++ test/TestMainframeStake.js
@@ -4,22 +4,37 @@ const utils = require('./utils.js')
 
 contract('MainframeStake', (accounts) => {
 
-  it('should deposits tokens', async () => {
+  it('should deposit tokens', async () => {
     const tokenContract = await MainframeToken.deployed()
     const stakeContract = await MainframeStake.deployed()
-    await tokenContract.approve(stakeContract.address, 100, { from: accounts[0], value: 0, gas: 3000000 })
-    await stakeContract.deposit(100, { from: accounts[0], value: 0, gas: 3000000 })
+    const maxDeposit = await stakeContract.maxDeposit()
+    await tokenContract.approve(stakeContract.address, maxDeposit, { from: accounts[0], value: 0, gas: 3000000 })
+    await stakeContract.deposit(maxDeposit, { from: accounts[0], value: 0, gas: 3000000 })
     const totalStaked = await stakeContract.totalStaked()
     const stakersBalance = await stakeContract.balanceOf(accounts[0])
-    assert.equal(stakersBalance, 100)
-    assert.equal(totalStaked, 100)
+    assert.equal(stakersBalance.toNumber(), maxDeposit)
+    assert.equal(totalStaked.toNumber(), maxDeposit)
+    // reset
+    await stakeContract.withdraw(maxDeposit, { from: accounts[0], value: 0, gas: 3000000 })
   })
 
-  it('should fail to despoit tokens if balance too low', async () => {
+  it('should fail to deposit tokens if balance too low', async () => {
     const tokenContract = await MainframeToken.deployed()
     const stakeContract = await MainframeStake.deployed()
-    await tokenContract.transfer(accounts[1], 100, { from: accounts[0], value: 0, gas: 3000000 })
-    await tokenContract.approve(stakeContract.address, 100, { from: accounts[0], value: 0, gas: 3000000 })
+    const maxDeposit = await stakeContract.maxDeposit()
+    await tokenContract.approve(stakeContract.address, maxDeposit, { from: accounts[1], value: 0, gas: 3000000 })
+    const didFail = await utils.expectAsyncThrow(async () => {
+      await stakeContract.deposit(maxDeposit, { from: accounts[1], value: 0, gas: 3000000 })
+    })
+    assert(didFail)
+  })
+
+  it('should fail to deposit tokens if it pushes balance over deposit limit', async () => {
+    const tokenContract = await MainframeToken.deployed()
+    const stakeContract = await MainframeStake.deployed()
+    const maxDeposit = await stakeContract.maxDeposit()
+    const deposit = maxDeposit + 5
+    await tokenContract.approve(stakeContract.address, deposit, { from: accounts[0], value: 0, gas: 3000000 })
     const didFail = await utils.expectAsyncThrow(async () => {
       await stakeContract.deposit(200, { from: accounts[0], value: 0, gas: 3000000 })
     })
@@ -28,11 +43,21 @@ contract('MainframeStake', (accounts) => {
 
   it('should allow withdraw if balance available', async () => {
     const stakeContract = await MainframeStake.deployed()
-    await stakeContract.withdraw(100, { from: accounts[0], value: 0, gas: 3000000 })
+    const tokenContract = await MainframeToken.deployed()
+    const maxDeposit = await stakeContract.maxDeposit()
+
+    await tokenContract.approve(stakeContract.address, maxDeposit, { from: accounts[0], value: 0, gas: 3000000 })
+    await stakeContract.deposit(maxDeposit, { from: accounts[0], value: 0, gas: 3000000 })
     const totalStaked = await stakeContract.totalStaked()
     const stakersBalance = await stakeContract.balanceOf(accounts[0])
-    assert.equal(stakersBalance, 0)
-    assert.equal(0, totalStaked)
+    assert.equal(stakersBalance, maxDeposit.toNumber())
+    assert.equal(totalStaked, maxDeposit.toNumber())
+
+    await stakeContract.withdraw(maxDeposit, { from: accounts[0], value: 0, gas: 3000000 })
+    const totalStakedAfter = await stakeContract.totalStaked()
+    const stakersBalanceAfter = await stakeContract.balanceOf(accounts[0])
+    assert.equal(totalStakedAfter, 0)
+    assert.equal(0, stakersBalanceAfter)
   })
 
   it('should fail withdraw if balance too low', async () => {
@@ -46,10 +71,21 @@ contract('MainframeStake', (accounts) => {
   it('should check address has stake', async () => {
     const tokenContract = await MainframeToken.deployed()
     const stakeContract = await MainframeStake.deployed()
-    await tokenContract.approve(stakeContract.address, 100, { from: accounts[0], value: 0, gas: 3000000 })
-    await stakeContract.deposit(100, { from: accounts[0], value: 0, gas: 3000000 })
+    const maxDeposit = await stakeContract.maxDeposit()
+    await tokenContract.approve(stakeContract.address, maxDeposit, { from: accounts[0], value: 0, gas: 3000000 })
+    await stakeContract.deposit(maxDeposit, { from: accounts[0], value: 0, gas: 3000000 })
     const hasStake = await stakeContract.hasStake(accounts[0])
-    assert.equal(true, hasStake)
+    assert(hasStake)
+  })
+
+  it('should allow owner to update max deposit', async () => {
+    const tokenContract = await MainframeToken.deployed()
+    const stakeContract = await MainframeStake.deployed()
+    const initialMaxDeposit = await stakeContract.maxDeposit()
+    assert(initialMaxDeposit !== 10)
+    await stakeContract.setMaxDeposit(10, { from: accounts[0], value: 0, gas: 3000000 })
+    const updatedMaxDeposit = await stakeContract.maxDeposit()
+    assert.equal(updatedMaxDeposit, 10)
   })
 
 })
