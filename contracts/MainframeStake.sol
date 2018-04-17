279f05aa8a427e46529125c1f278d9f57a5c6265
Miloš Mošić <mosic.milos@gmail.com>
2018-04-18 00:19:07 +0200

    Combine staking and escrow contracts into one

--- contracts/MainframeStake.sol
+++ contracts/MainframeStake.sol
@@ -1,11 +1,15 @@
 pragma solidity ^0.4.21;
 
-import "./MainframeEscrow.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "./StakeInterface.sol";
 
 contract MainframeStake is Ownable, StakeInterface {
   using SafeMath for uint256;
-  MainframeEscrow escrow;
+  uint256 public totalDepositBalance;
+  mapping (address => uint256) public balances;
+  ERC20 token;
 
   uint256 public requiredStake;
 
@@ -16,8 +20,8 @@ contract MainframeStake is Ownable, StakeInterface {
 
   mapping (address => Staker) public whitelist; // map of whitelisted addresses for efficient hasStaked check
 
-  function MainframeStake(address escrowAddress) public {
-    escrow = MainframeEscrow(escrowAddress);
+  function MainframeStake(address tokenAddress) public {
+    token = ERC20(tokenAddress);
     requiredStake = 1 ether; // ether = 10^18
   }
 
@@ -27,7 +31,7 @@ contract MainframeStake is Ownable, StakeInterface {
     whitelist[whitelistAddress].stakerAddress = msg.sender;
     whitelist[whitelistAddress].stakedAmount = requiredStake;
 
-    escrow.deposit(msg.sender, requiredStake);
+    deposit(msg.sender, requiredStake);
     emit Staked(msg.sender);
     return true;
   }
@@ -38,16 +42,33 @@ contract MainframeStake is Ownable, StakeInterface {
     uint256 stakedAmount = whitelist[whitelistAddress].stakedAmount;
     delete whitelist[whitelistAddress];
 
-    escrow.withdraw(msg.sender, stakedAmount);
+    withdraw(msg.sender, stakedAmount);
     emit Unstaked(msg.sender);
   }
 
-  function balanceOf(address owner) external view returns (uint256 balance) {
-    return escrow.balanceOf(owner);
+  function deposit(address _address, uint256 depositAmount) private returns (bool success) {
+    token.transferFrom(_address, this, depositAmount);
+    balances[_address] = balances[_address].add(depositAmount);
+    totalDepositBalance = totalDepositBalance.add(depositAmount);
+    emit Deposit(_address, depositAmount, balances[_address]);
+    return true;
+  }
+
+  function withdraw(address _address, uint256 withdrawAmount) private returns (bool success) {
+    require(balances[_address] >= withdrawAmount);
+    token.transfer(_address, withdrawAmount);
+    balances[_address] = balances[_address].sub(withdrawAmount);
+    totalDepositBalance = totalDepositBalance.sub(withdrawAmount);
+    emit Withdrawal(_address, withdrawAmount, balances[_address]);
+    return true;
+  }
+
+  function balanceOf(address _address) external view returns (uint256 balance) {
+    return balances[_address];
   }
 
   function totalStaked() external view returns (uint256) {
-    return escrow.totalDepositBalance();
+    return totalDepositBalance;
   }
 
   function hasStake(address _address) external view returns (bool) {
@@ -63,20 +84,36 @@ contract MainframeStake is Ownable, StakeInterface {
     requiredStake = value;
   }
 
-  function getEscrowAddress() public view returns (address) {
-    return address(escrow);
+  function refundBalances(address[] addresses) public onlyOwner {
+    for (uint256 i = 0; i < addresses.length; i++) {
+      address _address = addresses[i];
+      require(balances[_address] > 0);
+      token.transfer(_address, balances[_address]);
+      totalDepositBalance = totalDepositBalance.sub(balances[_address]);
+      emit RefundedBalance(_address, balances[_address]);
+      balances[_address] = 0;
+    }
   }
 
-  function emergencyERC20Drain(ERC20 token) public onlyOwner {
+  function emergencyERC20Drain(ERC20 _token) public onlyOwner {
     // owner can drain tokens that are sent here by mistake
-    uint256 amount = token.balanceOf(this);
-    token.transfer(owner, amount);
+    uint256 drainAmount;
+    if (address(_token) == address(token)) {
+      drainAmount = _token.balanceOf(this).sub(totalDepositBalance);
+    } else {
+      drainAmount = _token.balanceOf(this);
+    }
+    _token.transfer(owner, drainAmount);
   }
 
   function destroy() external onlyOwner {
+    require(token.balanceOf(this) == 0);
     selfdestruct(owner);
   }
 
   event Staked(address indexed owner);
   event Unstaked(address indexed owner);
+  event Deposit(address indexed _address, uint256 depositAmount, uint256 balance);
+  event Withdrawal(address indexed _address, uint256 withdrawAmount, uint256 balance);
+  event RefundedBalance(address indexed _address, uint256 refundAmount);
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,12 +2,9 @@
 
 const MainframeToken = artifacts.require('MainframeToken')
 const MainframeStake = artifacts.require('MainframeStake')
-const MainframeEscrow = artifacts.require('MainframeEscrow')
 
 module.exports = (deployer, network) => {
   deployer.deploy(MainframeToken).then(() => {
-    return deployer.deploy(MainframeEscrow, MainframeToken.address)
-  }).then(() => {
-    return deployer.deploy(MainframeStake, MainframeEscrow.address)
+    return deployer.deploy(MainframeStake, MainframeToken.address)
   })
 }
