62b722d15d96e6a6fc2d4174f8d3e74fb3d8010d
Adam Clarke <adam@mainframe.co>
2018-04-26 20:36:48 +0100

    rename token migration

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,8 +1,6 @@
 /* global artifacts */
 
 const MainframeToken = artifacts.require('MainframeToken')
-const MainframeStake = artifacts.require('MainframeStake')
-const MainframeEscrow = artifacts.require('MainframeEscrow')
 
 module.exports = (deployer, network) => {
   deployer.deploy(MainframeToken)
