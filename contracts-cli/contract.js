68e9cc4f1c70a2d2f1e419af216669b7cd653878
Adam Clarke <adam@mainframe.co>
2018-05-08 19:21:58 +0100

    ens contracts for cli

--- contracts-cli/contract.js
+++ contracts-cli/contract.js
@@ -4,6 +4,7 @@ const { utils } = require('web3')
 const config = require('./config')
 const customHandlers = require('./customHandlers')
 const { log } = require('./cli-utils')
+const namehash = require('eth-ens-namehash')
 const { GAS_LIMIT, GAS_PRICE } = require('./constants')
 
 const callCustomHandler = (contractName, method, web3Contract, ethNetwork, account) => {
@@ -52,12 +53,14 @@ const callMethod = async (
       name: m.name,
       message: `Enter ${m.name} (${m.type}): `,
     }
+    // TODO: Ask extra questions inline
+
     // If uint, ask if it needs unit conversion
     if (m.type.slice(0, 4) === 'uint') {
       q.extraQuestions = [
         {
           type: 'confirm',
-          name: 'confirm',
+          name: 'confirmUnitConvert',
           message: `Do you need to convert unit? `,
         },
         {
@@ -68,8 +71,22 @@ const callMethod = async (
         {
           type: 'list',
           name: 'conversion',
-          message: `Conversaion type: `,
+          message: `Conversion type: `,
           choices: ['fromWei', 'toWei'],
+        }
+      ]
+    } else if (m.type === 'bytes32') {
+      q.extraQuestions = [
+        {
+          type: 'confirm',
+          name: 'confirmTextToData',
+          message: `Do you need to convert text to data? `,
+        },
+        {
+          type: 'list',
+          name: 'encodeType',
+          message: `Hash type: `,
+          choices: ['nameHash', 'sha3', 'toHex'],
         }
       ]
     }
@@ -82,15 +99,22 @@ const callMethod = async (
     let arg = answers[q.name]
     if (q.extraQuestions) {
       const extraAnswers = await prompt(q.extraQuestions.slice(0, 1))
-      if (extraAnswers.confirm) {
+      if (extraAnswers.confirmUnitConvert) {
         const convertAnswers = await prompt(q.extraQuestions.slice(1, 3))
         arg = utils[convertAnswers.conversion](arg, convertAnswers.decimals)
+      } else if (extraAnswers.confirmTextToData) {
+        const dataAnswers = await prompt(q.extraQuestions.slice(1, 2))
+        if (dataAnswers.encodeType === 'nameHash') {
+          arg = namehash.hash(arg)
+        } else {
+          arg = utils[dataAnswers.encodeType](arg)
+        }
       }
     }
     args.push(arg)
   }
-
-  const methodType = abiMethod.stateMutability === 'view' ? 'call' : 'send'
+  const readOnly = abiMethod.stateMutability === 'view' || abiMethod.constant === true
+  const methodType = readOnly ? 'call' : 'send'
 
   if (methodType === 'send') {
     const validateMessage = `
